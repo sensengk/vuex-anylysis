473ab9e7ef5318d8d821b353fd9f66a6899164a6
吴森 <wusen@wusendeMacBook-Pro.local>
2018-12-18 23:47:13 +0800

    store comments

--- src/store.js
+++ src/store.js
@@ -6,11 +6,11 @@ import { forEachValue, isObject, isPromise, assert } from './util'
 let Vue // bind on install
 export class Store {
     constructor(options = {}) {
-            console.log(Store.constructor)
-                // Auto install if it is not done yet and `window` has `Vue`.
-                // To allow users to avoid auto-installation in some cases,
-                // this code should be placed here. See #731
-                // 如果window.vue存在的话，安装vue.  可能用于script
+            debugger
+            // Auto install if it is not done yet and `window` has `Vue`.
+            // To allow users to avoid auto-installation in some cases,
+            // this code should be placed here. See #731
+            // 如果window.vue存在的话，安装vue.  可能用于script
             if (!Vue && typeof window !== 'undefined' && window.Vue) {
                 install(window.Vue)
             }
@@ -27,15 +27,25 @@ export class Store {
             } = options
 
             // store internal state
-            // store 内部的状态
+            // 标志一个提交状态， vuex中state的修改能只能在mutation的回调函数中。而不能在外部随意修改state.
             this._committing = false
+
+            // 用来存储用户定义的所有actions
             this._actions = Object.create(null)
             this._actionSubscribers = []
+
+            // 用来存储用户定义所有的 mutatins
             this._mutations = Object.create(null)
+
+            // 用来存储用户定义所有的 getters
             this._wrappedGetters = Object.create(null)
             this._modules = new ModuleCollection(options)
             this._modulesNamespaceMap = Object.create(null)
+
+            //用来存储所有对 mutation 变化的订阅者。
             this._subscribers = []
+
+            // 主要是利用 Vue 实例方法 $watch 来观测变化的
             this._watcherVM = new Vue()
 
             // bind commit and dispatch to self
@@ -60,6 +70,7 @@ export class Store {
 
             // initialize the store vm, which is responsible for the reactivity
             // (also registers _wrappedGetters as computed properties)
+            // resetStoreVM 方法是初始化 store._vm
             resetStoreVM(this, state)
 
             // apply plugins
@@ -95,7 +106,6 @@ export class Store {
         const mutation = { type, payload }
             // 获取加工成数组的mutations
         const entry = this._mutations[type]
-        debugger
         if (!entry) {
             if (process.env.NODE_ENV !== 'production') {
                 console.error(`[vuex] unknown mutation type: ${type}`)
@@ -104,12 +114,13 @@ export class Store {
         }
         // 根据获取的mutations, 进行执行。传入payload
         this._withCommit(() => {
-                // 遍历包含mutation函数的数组，然后把payload,传进去。
-                entry.forEach(function commitIterator(handler) {
-                    handler(payload)
-                })
+            // 遍历包装之后的mutaions，然后把payload,传进去。
+            entry.forEach(function commitIterator(handler) {
+                handler(payload)
             })
-            // 执行发布者， 把mutation和当前的state 发布出去。
+        })
+
+        // 原来 Vuex 的 Store 实例提供了 subscribe API 接口，它的作用是订阅（注册监听） store 的 mutation
         this._subscribers.forEach(sub => sub(mutation, this.state))
 
         if (
@@ -148,7 +159,7 @@ export class Store {
     }
 
     /**
-     * @desc 插入到订阅者数组里面， 返回一个删除该函数的方法。
+     * @desc 接受的参数是一个回调函数 把这个回调函数保存到this._subscribers上。 并返回一个函数。
      * @param {fn} fn 
      * @return {Function}
      */
@@ -165,6 +176,13 @@ export class Store {
         return genericSubscribe(fn, this._actionSubscribers)
     }
 
+    /**
+     * @desc watch 作用是响应式的监测一个 getter 方法的返回值，当值改变时调用回调。
+     * getter 接收 store 的 state 作为唯一参数
+     * @param {Function} getter 
+     * @param {Function} cb 
+     * @param {*} options 
+     */
     watch(getter, cb, options) {
         if (process.env.NODE_ENV !== 'production') {
             assert(typeof getter === 'function', `store.watch only accepts a function.`)
@@ -173,7 +191,7 @@ export class Store {
     }
 
     /**
-     * @desc 传入的state来替代vm上的state, 进行数据变化。
+     * @desc 传入的state来替代vm上的state, 进行数据变化。replaceState的作用是替换整个 rootState 一般在用于调试
      * @param {Object} state 
      */
     replaceState(state) {
@@ -182,6 +200,12 @@ export class Store {
         })
     }
 
+    /**
+     * @desc 注册一个模块。
+     * @param {String|Array} path 
+     * @param {Object} rawModule 
+     * @param {*} options 
+     */
     registerModule(path, rawModule, options = {}) {
         // 如果传入的路径是String, 把他转化为数组。
         if (typeof path === 'string') path = [path]
@@ -190,13 +214,19 @@ export class Store {
             assert(Array.isArray(path), `module path must be a string or an Array.`)
             assert(path.length > 0, 'cannot register the root module by using registerModule.')
         }
-
+        // 根据相关的路径注册这个模块
         this._modules.register(path, rawModule)
+
+        // 安装模块，并且重新重置modules
         installModule(this, this.state, path, this._modules.get(path), options.preserveState)
             // reset store to update getters...
         resetStoreVM(this, this.state)
     }
 
+    /**
+     * @desc 移除某个模块。
+     * @param {String|Array} path 
+     */
     unregisterModule(path) {
         if (typeof path === 'string') path = [path]
 
@@ -204,21 +234,28 @@ export class Store {
             assert(Array.isArray(path), `module path must be a string or an Array.`)
         }
 
+        // 模块本身解注册。
         this._modules.unregister(path)
+
+        // 在相关的路径删除state
         this._withCommit(() => {
             const parentState = getNestedState(this.state, path.slice(0, -1))
             Vue.delete(parentState, path[path.length - 1])
         })
+
+        // 重新设置Store
         resetStore(this)
     }
 
     hotUpdate(newOptions) {
+        // 更新模块上的数据， 然后重新设置Store
         this._modules.update(newOptions)
         resetStore(this, true)
     }
 
     /**
-     * @desc committing 提交， 函数执行前为true, 执行后false, 截流。 
+     * @desc vuex中所有对state的更改都用_withcommit函数包装，保证在同步修改state的过程中，this._commiting 为true.
+     *  这样当我们观测state的变化， 如果this._commiting不为true.则能检查这个状态的修改是有问题的。 
      * @param {*} fn 接受一个函数，并且执行
      */
     _withCommit(fn) {
@@ -260,9 +297,12 @@ function resetStore(store, hot) {
 }
 
 function resetStoreVM(store, state, hot) {
+    debugger
     const oldVm = store._vm
 
     // bind store public getters
+    // 绑定 store的公共getters
+    // 通过defineProperty定义getters的一些属性。 而get
     store.getters = {}
     const wrappedGetters = store._wrappedGetters
     const computed = {}
@@ -279,7 +319,12 @@ function resetStoreVM(store, state, hot) {
     // suppress warnings just in case the user has added
     // some funky global mixins
     const silent = Vue.config.silent
+
+    // 临时改变silent
     Vue.config.silent = true
+
+    // 一个内置的vue实例。
+    // 计算属性的方式存储了 store 的 getters
     store._vm = new Vue({
         data: {
             $$state: state
@@ -290,6 +335,8 @@ function resetStoreVM(store, state, hot) {
 
     // enable strict mode for new vm
     if (store.strict) {
+
+        // 在严格模式下，监控state变化，必须通过mutations
         enableStrictMode(store)
     }
 
@@ -297,10 +344,12 @@ function resetStoreVM(store, state, hot) {
         if (hot) {
             // dispatch changes in all subscribed watchers
             // to force getter re-evaluation for hot reloading.
+            //  由于这个函数每次都会创建新的 Vue 实例并赋值到 store._vm 上， 那么旧的 _vm 对象的状态设置为 null， 
             store._withCommit(() => {
                 oldVm._data.$$state = null
             })
         }
+        // 调用 $destroy 方法销毁这个旧的 _vm 对象。
         Vue.nextTick(() => oldVm.$destroy())
     }
 }
@@ -326,7 +375,9 @@ function installModule(store, rootState, path, module, hot) {
     }
 
     // set state
+    /// 设置级联state   
     if (!isRoot && !hot) {
+        // 获取父状态
         const parentState = getNestedState(rootState, path.slice(0, -1))
         const moduleName = path[path.length - 1]
         store._withCommit(() => {
@@ -444,19 +495,28 @@ function makeLocalGetters(store, namespace) {
 }
 
 /**
- * 
+ * @desc 接受了四个参数， 第一个是store对象， 第二个是类型， 第三个是handler , 第四个是当前模块
  * @param {Object} store 
  * @param {String} type 
  * @param {Function} handler 
  * @param {*} local 
  */
 function registerMutation(store, type, handler, local) {
+    // handler操作符是在mutations 里面写的，接好连个参数， 第一个是state, 第二个是payload,
+    // 把mutations 修改包装，注册写到_mutations 里面。 然后在commit的时候执行， 只接受payload 
     const entry = store._mutations[type] || (store._mutations[type] = [])
     entry.push(function wrappedMutationHandler(payload) {
         handler.call(store, local.state, payload)
     })
 }
 
+/**
+ * @desc 接受了四个参数， 第一个是store对象， 第二个是类型， 第三个是handler , 第四个是当前模块
+ * @param {Object} store 
+ * @param {String} type 
+ * @param {Function} handler 
+ * @param {*} local 
+ */
 function registerAction(store, type, handler, local) {
     const entry = store._actions[type] || (store._actions[type] = [])
     entry.push(function wrappedActionHandler(payload, cb) {
@@ -469,9 +529,13 @@ function registerAction(store, type, handler, local) {
             rootState: store.state
         }, payload, cb)
         if (!isPromise(res)) {
+            //  如果不是promsie,加工成promise
             res = Promise.resolve(res)
         }
+
+        // Vuex devtools 开启的时候，我们才能捕获 promise 的过程！
         if (store._devtoolHook) {
+            // 处理里面可能存在的报错信息。
             return res.catch(err => {
                 store._devtoolHook.emit('vuex:error', err)
                 throw err
@@ -482,6 +546,14 @@ function registerAction(store, type, handler, local) {
     })
 }
 
+/**
+ * @desc 注册getter, 把所有的getter封装放到_wrappedGetters里面。包装了一层。 
+ *   包转的_wrappedGetters 在resetStoreVM里面执行。
+ * @param {Object} store 
+ * @param {String} type 
+ * @param {Function} rawGetter 
+ * @param {Object} local 
+ */
 function registerGetter(store, type, rawGetter, local) {
     if (store._wrappedGetters[type]) {
         if (process.env.NODE_ENV !== 'production') {
@@ -490,6 +562,7 @@ function registerGetter(store, type, rawGetter, local) {
         return
     }
     store._wrappedGetters[type] = function wrappedGetter(store) {
+        // 向外返回的是state， getters, rootState, rootgetters.
         return rawGetter(
             local.state, // local state
             local.getters, // local getters
@@ -498,7 +571,12 @@ function registerGetter(store, type, rawGetter, local) {
         )
     }
 }
-
+/**
+ * @desc 监测 store._vm.state 的变化，看看 state 的变化是否通过执行 mutation 的回调函数改变，
+ * 如果是外部直接修改 state，那么 store._committing 的值为 false，这样就抛出一条错误。
+ * 再次强调一下，Vuex 中对 state 的修改只能在 mutation 的回调函数里
+ * @param {Object} store 
+ */
 function enableStrictMode(store) {
     store._vm.$watch(function() { return this._data.$$state }, () => {
         if (process.env.NODE_ENV !== 'production') {
